6f65d83c4c647b03d577a452ec43cebef4664000
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-03-03 18:28:01 +0530

    pinal
    pratice_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'pinal/message_screen.dart';
+import 'pinal/pratice_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MessageScreen(),
+      home: const PraticeScreen(),
     );
   }
 }
