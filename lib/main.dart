f7f99716160b76ca8c2bebf4a849b8fd69a7e87f
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-03-03 15:41:21 +0530

    pinal
    explore_menu_with_filter_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'pinal/explore_restaurant_with_filter_screen.dart';
+import 'pinal/explore_menu_with_filter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ExploreRestaurantWithFilterScreen(),
+      home: const ExploreMenuWithFilterScreen(),
     );
   }
 }
