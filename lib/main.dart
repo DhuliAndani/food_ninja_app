c0ecd6caad5883b1d79de0a967597231585b5dde
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-03-03 20:18:53 +0530

    pinal
    chat_details_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'chirag/on_boarding_screen.dart';
+import 'pinal/chat_details_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const OnBoardingScreen(),
+      home: const ChatDetailsScreen(),
     );
   }
 }
