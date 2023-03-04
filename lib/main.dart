fbb749e9eef56ae65bbf6ec2a89cada428d0ca3d
Dhruvi Andani <dhruviandani0601@gmail.com>
2023-03-04 11:51:48 +0530

    dhruvi
    image add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //import 'dhuli/splash_screen.dart';
-import 'profile_scroll_mode_screen.dart';
+//import 'profile_scroll_mode_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      // home: const HomeScreen(),
     );
   }
 }
