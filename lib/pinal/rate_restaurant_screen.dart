e9b78316560426e86f1e1c5a00752124950028d0
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-03-05 14:15:17 +0530

    pinal
    rate_restaurant_screen

---
+++ lib/pinal/rate_restaurant_screen.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+import 'Common/finish_order_common.dart';
+import 'Common/finish_order_common_container.dart';
+
+class RateRestaurantScreen extends StatefulWidget {
+  const RateRestaurantScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RateRestaurantScreen> createState() => _RateRestaurantScreenState();
+}
+
+class _RateRestaurantScreenState extends State<RateRestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image(
+                image: AssetImage("assets/images/green.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 190, left: 80),
+                child: Image(
+                  image: AssetImage("assets/images/Image_3.png"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 400, left: 125),
+                child: Text(
+                  "Thank You!",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Merriweather-Bold",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 435, left: 100),
+                child: Text(
+                  "Enjoy Your Meal",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Merriweather-Bold",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 480, left: 105),
+                child: Text(
+                  "Please rate your Restaurant",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Rubik-Regular",
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 75, top: 520),
+                child: Row(
+                  children: [
+                    Image.asset("assets/images/Star_1.png"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Image.asset("assets/images/Star_2.png"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Image.asset("assets/images/Star_3.png"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Image.asset("assets/images/Star_4.png"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Image.asset("assets/images/Star_5.png"),
+                    SizedBox(
+                      width: 25,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 650),
+                child: FinishOrderCommonContainer(
+                  image: "assets/images/edit Icon.png",
+                  text: "Leave feedback",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 730, right: 20),
+                child: Row(
+                  children: [
+                    FinishOrderCommon(
+                      height: 57,
+                      width: 228,
+                      color: Color(0xFF53E88B),
+                      text: "Submit",
+                      SecondColor: Color(0xFFFEFEFF),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    FinishOrderCommon(
+                      height: 57,
+                      width: 82,
+                      color: Color(0xFF252525),
+                      text: "Skip",
+                      SecondColor: Color(0xFF53E88B),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
