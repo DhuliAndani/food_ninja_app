5fb8d313e2bde52a505dd822a37b44ded8791395
Dhruvit Jetani <dhruvitjetani3967@gmail.com>
2023-03-06 11:33:38 +0530

    dhruvit

--- lib/dhruvit/track_order_screen.dart
+++ lib/dhruvit/track_order_screen.dart
@@ -42,7 +42,8 @@ class _TrackOrderScreenState extends State<TrackOrderScreen> {
               Padding(
                 padding: const EdgeInsets.only(top: 540, left: 15, right: 15),
                 child: Container(
-                 
+                  height: 240,
+                  // width: 342,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     color: const Color(0xFF0D0D0D),
@@ -52,15 +53,147 @@ class _TrackOrderScreenState extends State<TrackOrderScreen> {
                       Image.asset(
                         "assets/images/Map_Track_Pattern.png",
                       ),
-                      // const Text(
-                      //       "Track Orders",
-                      //       style: TextStyle(
-                      //         fontFamily: "BentonSans",
-                      //         fontSize: 17,
-                      //         color: Colors.white,
-                      //         fontWeight: FontWeight.w400,
-                      //       ),
-                      //     ),
+                      const Padding(
+                        padding: EdgeInsets.only(left: 20, top: 20),
+                        child: Text(
+                          "Track Orders",
+                          style: TextStyle(
+                            fontFamily: "BentonSans",
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(right: 10, left: 10, top: 50),
+                        child: Container(
+                          height: 87,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: const Color(0xFF252525)),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Image.asset(
+                                "assets/images/Profile_Order.png",
+                                height: 64,
+                                width: 64,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  const Text(
+                                    "Mr Kemplas",
+                                    style: TextStyle(
+                                      fontFamily: "BentonSans",
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Image.asset(
+                                        "assets/images/Icon_maps.png",
+                                        height: 12,
+                                        width: 12,
+                                      ),
+                                      const SizedBox(
+                                        width: 7,
+                                      ),
+                                      const Text(
+                                        "25 minutes on the way",
+                                        style: TextStyle(
+                                          fontFamily: "BentonSans",
+                                          fontSize: 14,
+                                          color:
+                                              Color.fromARGB(60, 255, 255, 255),
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, top: 155),
+                        child: Container(
+                          height: 68,
+                          width: 240,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xFF53E88B),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 95),
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  "assets/images/Call_logo.png",
+                                  height: 18,
+                                  width: 18,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text(
+                                  "Call",
+                                  style: TextStyle(
+                                    fontFamily: "BentonSans",
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 257, top: 155),
+                        child: Container(
+                          height: 68,
+                          width: 68,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xFF252525),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadiusDirectional.circular(10),
+                                color: const Color(0xFF53E88B),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: Image.asset(
+                                  "assets/images/Down_Arrow_Ios.png",
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ],
                   ),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'dhruvit/track_order_screen.dart';
+import 'dhruvit/detail_product_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-home: const TrackOrderScreen(),
+home: const DetailProductScreen(),
     );
   }
 }
