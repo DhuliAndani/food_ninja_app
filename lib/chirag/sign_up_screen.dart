cd81a4b4547d8884def8d8ded69e722e31480f4f
Chirag Ranoliya <chiragranoliya33@gmail.com>
2023-03-06 11:35:58 +0530

    chirag
    three screen

--- lib/chirag/sign_up_screen.dart
+++ lib/chirag/sign_up_screen.dart
@@ -12,39 +12,47 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF0D0D0D),
-      body: Stack(
-        children: const [
-          Image(
-            image: AssetImage(
-              "assets/images/greenback.png",
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 100, left: 100),
-            child: Image(
-              image: AssetImage(
-                "assets/images/Logo.png",
+      body: Column(
+        children: [
+          Stack(
+            children: const [
+              Image(
+                image: AssetImage(
+                  "assets/images/greenback.png",
+                ),
               ),
-              height: 140,
-            ),
+              Padding(
+                padding: EdgeInsets.only(top: 100, left: 100),
+                child: Image(
+                  image: AssetImage(
+                    "assets/images/Logo.png",
+                  ),
+                  height: 140,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 250, left: 90),
+                child: Text(
+                  'FoodNinja',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: Color(0xff53E88B)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 300, left: 110),
+                child: Text(
+                  'Deliever Favorite Food',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 250, left: 90),
-            child: Text(
-              'FoodNinja',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40,
-                  color: Color(0xff53E88B)),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 300, left: 110),
-            child: Text(
-              'Deliever Favorite Food',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
+          Text(
+            'Login To Your Account',
+            style: TextStyle(color: Colors.blue),
+          )
         ],
       ),
     );
