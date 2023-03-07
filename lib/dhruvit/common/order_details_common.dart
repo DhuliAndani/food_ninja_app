fab59b7a3b4ba2f0547c385b06e00763e0b532a7
Chirag Ranoliya <chiragranoliya33@gmail.com>
2023-03-07 11:58:04 +0530

    chirag

--- lib/dhruvit/common/order_details_common.dart
+++ lib/dhruvit/common/order_details_common.dart
@@ -24,11 +24,6 @@ class OrderDetailsCommon extends StatelessWidget {
                 width: 62,
                 child: image,
               ),
-              SizedBox(
-                height: 62,
-                width: 62,
-                child: image,
-              ),
               Padding(
                 padding: const EdgeInsets.only(left: 10),
                 child: Column(
