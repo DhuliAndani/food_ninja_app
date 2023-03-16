afb3c25797be606a64c9d6db1b3d442022068770
Pinal trapasiya <pinaltrapasiya2213@gmail.com>
2023-03-16 21:19:48 +0530

    pinal
    home_screen

--- lib/pinal/home_screen.dart
+++ lib/pinal/home_screen.dart
@@ -14,6 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //
       backgroundColor: Colors.transparent,
       body: Column(
         children: [
