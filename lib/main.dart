51f7e87435107fa58e59b88fa5115f1a6b8c02a0
Chirag Ranoliya <chiragranoliya33@gmail.com>
2023-03-17 12:12:22 +0530

    chirag
    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_ninja_app/dhuli/profile_screen.dart';
 import 'package:food_ninja_app/chirag/sign_in_screen.dart';
-
-import 'pinal/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +15,7 @@ class MyApp extends StatelessWidget {
 
       title: "flutter demo",
       // home: const SignInScreen(),
-      home: const ProfileScreen(),
+      home: const SignInScreen(),
     );
   }
 }
