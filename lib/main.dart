a99ed2c7343be8b71d712c4915b80f19ab18fb2d
Dhruvit Jetani <dhruvitjetani3967@gmail.com>
2023-03-17 11:57:58 +0530

    Dhruvit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       title: "flutter demo",
-      // home: const SignInScreen(),
+      
       home: SignupSuccessNotificationScreen(),
     );
   }
